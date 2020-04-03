0bfaa2cf6f874f6d0f10e576597a425426cdfc70
Galen Emanuel <galen.emanuel.fencing@gmail.com>
2020-04-03 12:12:01 -0400

    added button wireframe

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -4,4 +4,13 @@ import { storiesOf } from '@storybook/react';
 import Button from './Button';
 
 storiesOf('Button', module)
-    .add('Primary', () => <Button label="Primary" />) 
+    .add('Primary', () => <Button type="primary" label="Primary" />)
+    .add('Danger', () => <Button type="danger" label="Danger" />)
+    .add('Success', () => <Button type="success" label="Success" />)
+    .add('Warning', () => <Button type="warning" label="Warning" />)
+    .add('Large Primary', () => <Button type="primary" label="Large Primary Button" large/>)
+    .add('Large Danger', () => <Button type="danger" label="Large Danger Button" large/>)
+    .add('Large Success', () => <Button type="success" label="Large Success Button" large/>)
+    .add('Large Warning', () => <Button type="warning" label="Large Warning Button" large/>)
+
+
